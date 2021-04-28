5657462f5c2e52e54dc70c62b66f6cf3ac0b5d2f
Shavonne Cobb <shavonnecobb@Shavonnes-MBP.attlocal.net>
2021-04-28 15:54:24 -0500

    Last time cleaning up code

--- src/main/java/com/shavonne/TakeHomeChallenge/Model/JSON.java
+++ src/main/java/com/shavonne/TakeHomeChallenge/Model/JSON.java
@@ -5,11 +5,7 @@ import java.sql.Timestamp;
 public class JSON {
 
     private String message;
-    private Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-
-    public String getMessage() {
-        return message;
-    }
+    private final Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
     public void setMessage(String message) {
         this.message = message;
